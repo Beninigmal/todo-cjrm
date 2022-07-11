d5885129451ac84cdb6cca83bea5709a871958e6
Lucas Araújo <beninigmal@gmail.com>
2022-07-10 23:00:39 -0300

    Método create concluído inserindo novo todo em tela ao efetuar submit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const formTodo = document.querySelector('.form-add-todo')
+const ulContainer = document.querySelector('.todos-container')
+
+const createTodo = () => {
+    formTodo.addEventListener('submit', event => {
+        event.preventDefault()
+        const inputTodo = event.target.add.value.trim()
+    
+        if(inputTodo.length){
+            ulContainer.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${inputTodo}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+            `
+            event.target.reset()
+        }
+        console.log(inputTodo)
+    })
+}
+
+createTodo()
 
