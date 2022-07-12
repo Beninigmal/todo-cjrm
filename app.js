db2b901d2a20bd3709177c2bc966a0cbb44097b0
Beni nigmal <beninigmal@gmail.com>
2022-07-11 23:53:36 -0300

    Criado busca de todos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formTodo = document.querySelector(".form-add-todo");
 const ulContainer = document.querySelector(".todos-container");
+const formSearch = document.querySelector(".form-search input");
 
 const createTodo = () => {
   formTodo.addEventListener("submit", (event) => {
@@ -22,11 +23,30 @@ const createTodo = () => {
 const deleteTodo = () => {
   ulContainer.addEventListener("click", ({ target }) => {
     const clickedTodo = target;
-    if (Array.from(clickedTodo.classList).includes("delete")) {
+    const hasClassDelete = Array.from(clickedTodo.classList).includes("delete");
+    if (hasClassDelete) {
       clickedTodo.parentNode.remove();
     }
   });
 };
 
+const searchTodo = () => {
+  formSearch.addEventListener("input", (event) => {
+    const input = event.target.value.trim().toLowerCase();
+    const todos = Array.from(ulContainer.children);
+    todos.forEach((todo) => {
+      const todoContent = todo.textContent.toLowerCase();
+      if (!todoContent.includes(input)) {
+        todo.classList.add("hidden");
+        todo.classList.remove("d-flex");
+      } else {
+        todo.classList.add("d-flex");
+        todo.classList.remove("hidden");
+      }
+    });
+  });
+};
+
 createTodo();
 deleteTodo();
+searchTodo();
