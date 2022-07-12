6ac5a15155a05c133007671b4d4a3c9d4bd22459
Beni nigmal <beninigmal@gmail.com>
2022-07-11 23:07:16 -0300

    Criado método para deleção de todo

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-const formTodo = document.querySelector('.form-add-todo')
-const ulContainer = document.querySelector('.todos-container')
+const formTodo = document.querySelector(".form-add-todo");
+const ulContainer = document.querySelector(".todos-container");
 
 const createTodo = () => {
-    formTodo.addEventListener('submit', event => {
-        event.preventDefault()
-        const inputTodo = event.target.add.value.trim()
-    
-        if(inputTodo.length){
-            ulContainer.innerHTML += `
+  formTodo.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputTodo = event.target.add.value.trim();
+
+    if (inputTodo.length) {
+      ulContainer.innerHTML += `
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 <span>${inputTodo}</span>
                 <i class="far fa-trash-alt delete"></i>
             </li>
-            `
-            event.target.reset()
-        }
-        console.log(inputTodo)
-    })
-}
+            `;
+      event.target.reset();
+    }
+    console.log(inputTodo);
+  });
+};
 
-createTodo()
+const deleteTodo = () => {
+  ulContainer.addEventListener("click", ({ target }) => {
+    const clickedTodo = target;
+    if (Array.from(clickedTodo.classList).includes("delete")) {
+      clickedTodo.parentNode.remove();
+    }
+  });
+};
 
+createTodo();
+deleteTodo();
